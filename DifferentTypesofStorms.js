438285b2078728bc9030542c3c84eebe27233a9a
CaseyHeyrend <caseyheyrend.byui8@gmail.com>
2016-12-13 11:48:49 -0700

    Update DifferentTypesofStorms.js

--- DifferentTypesofStorms.js
+++ DifferentTypesofStorms.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
-  $("#winter_controls").on('click', 'span', function() {
-    $("#winter img").removeClass("opaque");
-
-    var newImage = $(this).index();
-
-    $("#winter img").eq(newImage).addClass("opaque");
-
-    $("#winter_controls span").removeClass("selected");
-    $(this).addClass("selected");
-  });
-});
+document.getElementById("searchButton").onclick = function() {
+  if (encodeURIComponent(document.getElementById("stormName").value) === '' || null) {
+    p = document.createElement("P");
+    p.innerHTML = "Please Enter a storm";
+    p.setAttribute('id', 'errorMessage');
+    document.getElementById('error').appendChild(p);
+    document.getElementById('error').className = 'error_found';
+    var errorExists = document.getElementById("errorMessage");
+    setTimeout(function(){
+    errorExists.parentNode.removeChild(errorExists);
+    document.getElementById('error').className = 'error';
+}, 5000);
+  } else {
+  getStormType(); 
+  }
+  }
